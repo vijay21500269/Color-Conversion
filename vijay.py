805fb0ca9856118154dc470f165ba3cc2d45ab78
vijay21500269 <94381788+vijay21500269@users.noreply.github.com>
2023-03-28 20:55:00 +0530

    Add files via upload

---
+++ vijay.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+house_color_image = cv2.imread('img.jpeg')
+cv2.imshow('Original image',house_color_image)
+#BGR 2 HSV
+hsv_image = cv2.cvtColor(house_color_image, cv2.COLOR_BGR2HSV)
+cv2.imshow('BGR2HSV',hsv_image)
+#RGB 2 HSV
+hsv_image1 = cv2.cvtColor(house_color_image, cv2.COLOR_RGB2HSV)
+cv2.imshow('RGB2HSV',hsv_image1)
+# BGR 2 GRAY
+gray_image = cv2.cvtColor(house_color_image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('BGR2GRAY',gray_image)
+# RGB 2 GRAY
+gray_image1 = cv2.cvtColor(house_color_image, cv2.COLOR_RGB2GRAY)
+cv2.imshow('RGB2GRAY',gray_image1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[2]:
+
+
+import cv2
+sun_color_image = cv2.imread('img.jpeg')
+cv2.imshow('Original image', sun_color_image)
+hsv_image = cv2.cvtColor(sun_color_image, cv2.COLOR_HSV2RGB)
+cv2.imshow('HSV2RGB' ,hsv_image )
+gray_image1 = cv2.cvtColor (sun_color_image, cv2.COLOR_HSV2BGR)
+cv2.imshow('HSV2BGR', gray_image1)
+cv2.waitKey(0)
+cv2. destroyAllWindows()
+
+
+# In[3]:
+
+
+import cv2
+sun_color_image = cv2.imread('img.jpeg')
+cv2.imshow('Original image', sun_color_image)
+gray_image1 = cv2.cvtColor (sun_color_image, cv2.COLOR_RGB2YCrCb)
+cv2.imshow('RGB2YCrCb', gray_image1)
+gray_image1 = cv2.cvtColor (sun_color_image, cv2.COLOR_BGR2YCrCb)
+cv2.imshow('BGR2YCrCb', gray_image1)
+cv2.waitKey(0)
+cv2. destroyAllWindows()
+
+
+# In[4]:
+
+
+import cv2
+image = cv2.imread('img.jpeg')
+blue=image[:,:,0]
+green=image[:,:,1]
+red=image[:,:,2]
+cv2.imshow('B-Channel',blue)
+cv2.imshow('G-Channel',green)
+cv2.imshow('R-Channel',red)
+merged_BGR=cv2.merge((blue,green,red))
+cv2.imshow('Merged BGR Image',merged_BGR)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
+
+
+# In[5]:
+
+
+import cv2
+image = cv2.imread('img.jpeg')
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+h,s,v = cv2.split(hsv)
+cv2.imshow('H',h)
+cv2.imshow('S',s)
+cv2.imshow('V',v)
+Merged_HSV = cv2.merge((h,s,v))
+cv2.imshow('Merged HSV Image',Merged_HSV)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
+
+
+# In[ ]:
+
+
+
+
